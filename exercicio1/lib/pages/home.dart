53ab22b6dd2942de1967ecdc4331a37dd1f91134
VictorBratfisch <victorbratfisch22@gmail.com>
2023-03-22 11:47:20 -0300

    finalizado

--- exercicio1/lib/pages/home.dart
+++ exercicio1/lib/pages/home.dart
@@ -25,47 +25,92 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            margin: const EdgeInsets.only(left: 10),
-            child: Row(
-              children: const [
-                SizedBox(
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(10, 16, 0, 0),
+                child: SizedBox(
                   width: 100,
                   height: 100,
                   child: Card(
                     child: Icon(Icons.euro),
                   ),
                 ),
-                SizedBox(
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 16, 0, 0),
+                child: SizedBox(
                   width: 270,
                   height: 100,
                   child: Card(
-                    child: Text('From'),
+                    child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'From',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              '\$3600.00',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.only(left: 10),
-            child: Row(
-              children: const [
-                SizedBox(
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: SizedBox(
                   width: 100,
                   height: 100,
                   child: Card(
+                    color: Color.fromARGB(255, 237, 99, 45),
                     child: Icon(Icons.attach_money),
                   ),
                 ),
-                SizedBox(
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 0, 0, 0),
+                child: SizedBox(
                   width: 270,
                   height: 100,
                   child: Card(
-                    child: Text('To'),
+                    color: const Color.fromARGB(255, 237, 99, 45),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'To',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            '\$3631.55',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
           Row(
             children: [
@@ -294,13 +339,13 @@ class HomePage extends StatelessWidget {
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 237, 99, 45),
+                    backgroundColor: const Color.fromARGB(255, 237, 99, 45),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(16.0),
                     ),
                     minimumSize: const Size(90, 90),
                   ),
-                  child: const Text('Icon'),
+                  child: const Icon(Icons.arrow_upward),
                 ),
               ),
             ],
