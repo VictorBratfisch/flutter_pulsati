db9e2d04100618df0816cef3cbc1227578d77183
VictorBratfisch <victorbratfisch22@gmail.com>
2023-03-30 17:47:09 -0300

    var

--- exercicio5/lib/home.dart
+++ exercicio5/lib/home.dart
@@ -11,17 +11,42 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: const [CustomSwitch()],
-      ),
-      body: const Center(
-        child: CustomSwitch(),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Home'),
+          actions: const [CustomSwitch()],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ]),
+          ),
+        ));
   }
 }
 
